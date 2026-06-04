4422fa311f7114140ce617bd13aebcfe2498966c
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-04 17:37:09 +0530

    dart-variables

--- dart_practice/main.dart
+++ dart_practice/main.dart
@@ -1,3 +1,20 @@
-void main() {
-  print("Hello Dart!");
+void main(){
+  // variables
+  String name = "Rakesh";
+
+  var x = 21;
+  int y = 33;
+
+  //also we can use dynamic
+  dynamic firstName = 'Rhynamo';
+
+  // const and final..
+  const age = 21;
+  final date = '10/10/2003';
+
+// declaring and not using
+var myName;
+print(myName);
+  myName = 'Rakesh';
+print(myName);
 }
