ea6f650574313b666302a561faa0675a7821e359
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-09 13:03:40 +0530

    logic

--- dart_practice/logic.dart
+++ dart_practice/logic.dart
@@ -1,10 +1,24 @@
 void main(){
   // logic if, if else, if else if
-    for(var i = 1; i <= 100; i++){
-      if(i % 2 == 0){
-        print('Number is ODD');
-      } else{
-        print('Number is Even');
-      }
+    // for(var i = 1; i <= 30; i++){
+    //   if(i % 2 == 0){
+    //     print('Number is ODD');
+    //   } else{
+    //     print('Number is Even');
+    //   }
+    // }
+
+    var secretNumber = 25;
+    var guess = 25;
+
+    if(secretNumber > guess){
+      print('Too Low');
+    } 
+    else if(secretNumber < guess){
+      print('Too High');
     }
+    else{
+      print('Your guess is right');
+    }
+
 }
