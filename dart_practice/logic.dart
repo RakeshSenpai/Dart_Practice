b11f23f711f3139600ece7cdaea6b23ec9e69e38
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-09 12:27:42 +0530

    logic

---
+++ dart_practice/logic.dart
@@ -0,0 +1,10 @@
+void main(){
+  // logic if, if else, if else if
+    for(var i = 1; i <= 100; i++){
+      if(i % 2 == 0){
+        print('Number is ODD');
+      } else{
+        print('Number is Even');
+      }
+    }
+}
