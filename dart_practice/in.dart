06c8d947cf7a36f05a1bff11b90d26e5bb7b4ea7
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-10 23:18:16 +0530

    user input

--- dart_practice/in.dart
+++ dart_practice/in.dart
@@ -3,7 +3,12 @@ import 'dart:io';
 void main(){
   // User Input
   print('Enter your name');
-  var name = stdin.readLineSync();
+  // Allow the user to enter their name
+  //and this is going to take a String.
+  // int num = stdin.readLineSync();//this doesnot works it throw errors
+
+  // var name = stdin.readLineSync();
+  String? name = stdin.readLineSync();
   print("Hello $name");
     
 }
