70235ac64404937153935f8c872ccdda5d057678
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-13 12:11:45 +0530

    User(Classes)

--- dart_practice/classes.dart
+++ dart_practice/classes.dart
@@ -1,7 +1,9 @@
 void main(){
   User p1 = User("Rakesh", 'af@gmail.com', 22);
   p1.showDate();
-
+  print(p1.name);
+  User p2 = User('Raechel', "rachel@email.com", 22);
+  p2.showDate();
 }
 
 class User{
@@ -18,5 +20,7 @@ class User{
     print("Name = $name");
     print("Email = $contact");
     print("Age = $age");
+
+    print("There is person name's {$name}, and the person's age is {$age}, and his contact {$contact}");
   }
 }
